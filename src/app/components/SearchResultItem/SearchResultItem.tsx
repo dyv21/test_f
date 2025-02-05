380c92da73c7a06ee7ab0d76665ef87affb4a8f2
dyv21 <denis@artkerr.com>
2025-02-05 17:29:14 +0300

    feat: fill SearchResultItem component

--- src/app/components/SearchResultItem/SearchResultItem.tsx
+++ src/app/components/SearchResultItem/SearchResultItem.tsx
@@ -3,7 +3,11 @@ import s from './SearchResultItem.module.css'
 export const SearchResultItem = () => {
   return (
     <div className={s.wrapper}>
-
+      <h3 className={s.title}>Stair Goblin - Mythological Creature</h3>
+      <div className={s.info}>
+        <p>Status: <span>Dead</span></p>
+        <p>Created: 04.11.2017</p>
+      </div>
 
     </div>
   )
