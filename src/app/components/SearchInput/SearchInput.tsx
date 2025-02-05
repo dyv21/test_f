1f3ce71e54892f5ba3ada0eec98742a949961f72
dyv21 <denis@artkerr.com>
2025-02-05 15:57:24 +0300

    feat: add label & span for SearchInput component

--- src/app/components/SearchInput/SearchInput.tsx
+++ src/app/components/SearchInput/SearchInput.tsx
@@ -3,7 +3,11 @@ import s from './SearchInput.module.css'
 export const SearchInput = () => {
     return (
       <div className={s.wrapper}>
-        <input className={s.input} type="text"  defaultValue={'Search characters...'}/>
+        <div className={s.form}>
+          <label htmlFor="search-form">Search form</label>
+          <input id={"search-form"} className={s.input} type="text" defaultValue={'Search characters...'}/>
+          <span>Found characters: 8</span>
+        </div>
       </div>
     )
 
