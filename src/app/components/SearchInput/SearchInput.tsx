29c9a2ccaf910f504af4f4833917ac209baffa3d
dyv21 <denis@artkerr.com>
2025-02-05 17:58:15 +0300

    feat: add localState for SearchInput

--- src/app/components/SearchInput/SearchInput.tsx
+++ src/app/components/SearchInput/SearchInput.tsx
@@ -1,14 +1,18 @@
 import s from './SearchInput.module.css'
+import {useState} from "react"
 
 export const SearchInput = () => {
-    return (
-      <header className={s.wrapper}>
-        <div className={s.form}>
-          <label htmlFor="search-form">Search form</label>
-          <input id={"search-form"} className={s.input} type="text" defaultValue={'Search characters...'}/>
-          <span>Found characters: 8</span>
-        </div>
-      </header>
-    )
+  const [find, setFind] = useState('Search characters...')
+
+  return (
+    <header className={s.wrapper}>
+      <div className={s.form}>
+        <label htmlFor="search-form">Search form</label>
+        <input id={"search-form"} className={s.input} type="text" value={find}
+               onChange={(e) => setFind(e.target.value)} onFocus={() => setFind("")}/>
+        <span>Found characters: 8</span>
+      </div>
+    </header>
+  )
 
 }
