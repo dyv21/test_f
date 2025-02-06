6053ae3920cf683d21b0160cf77a4f2fd071fb02
dyv21 <denis@artkerr.com>
2025-02-06 12:16:23 +0300

    feat: add error handliing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,27 +4,38 @@ import {SearchResult} from "../components/SearchResult/SearchResult.tsx"
 import {API} from "../api/api.ts"
 import {useState} from "react"
 
-type StatusType = 'idle' | 'loading' | 'error' | 'success'
+type StatusTypes = 'idle' | 'loading' | 'error' | 'success'
 
 
 function App() {
   const [characters, setCharacters] = useState([])
-  const [error, setError] = useState(null)
+  const [error, setError] = useState<string | null>(null)
   const [totalCount, setTotalCount] = useState(null)
-  const [status, setStatus] = useState<StatusType>('idle')
+  const [status, setStatus] = useState<StatusTypes>('idle')
 
   const sendQuery = (name:string) => {
     setStatus('loading')
+    setError(null)
     API.getCharactersByName(name)
       .then(res => {
-        setCharacters(res.data.results)
-        setTotalCount(res.data.info.count)
+        if (res.data.results.length === 0) {
+          setError('Nothing found try again')
+          setCharacters([])
+        } else  {
+          setCharacters(res.data.results)
+          setTotalCount(res.data.info.count)
+          setStatus('success')
+        }
       })
       .catch(err => {
-        setError(err)
+        if (err.response?.status === 404) {
+          setError("Nothing found try again");
+        } else {
+          setError("Error occurred");
+        }
+        setCharacters([]);
         setStatus('error')
       })
-      .finally(() => setStatus('success'))
   }
 
   return (
