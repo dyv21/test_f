972b7a2179f8315a73c74174075decd9ff74a825
dyv21 <denis@artkerr.com>
2025-02-06 12:07:45 +0300

    fix: issue with month format

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ export const dateFormatter = (date: string) => {
   const parsedDate = new Date(date);
 
   const day = parsedDate.getDate();
-  const month = parsedDate.getMonth()
+  const month = parsedDate.getMonth() + 1;
   const year = parsedDate.getFullYear();
 
 
