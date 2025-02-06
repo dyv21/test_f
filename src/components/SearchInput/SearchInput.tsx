390eadb350a6ff80ef8bfbc58f8ed99ffa427bbe
dyv21 <denis@artkerr.com>
2025-02-06 20:52:10 +0300

    fix: add a11y for search form

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -2,7 +2,7 @@ import s from './SearchInput.module.css'
 import {useState} from "react"
 
 type PropsType = {
-  sendQuery: (name:string) => void
+  sendQuery: (name: string) => void
   totalCount: number | null
 }
 
@@ -32,12 +32,16 @@ export const SearchInput = (props: PropsType) => {
 
   return (
     <header className={s.wrapper}>
-      <div className={s.form}>
-        <label htmlFor="search-form">Search form</label>
-        <input id={"search-form"} className={s.input} type="text" value={value}
-               onChange={onChangeHandler} onFocus={() => setValue('')} onKeyDown={onKeyDownHandler} autoFocus/>
+      <form className={s.form} onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor={"search-form"}>
+          <input name={"search-form"} id={"search-form"} className={s.input} type="text"
+                 value={value}
+                 onChange={onChangeHandler} onFocus={() => setValue('')}
+                 onKeyDown={onKeyDownHandler} autoFocus aria-label="Search form"/>
+        </label>
+
         {totalCount !== null && <span>Found characters: {totalCount}</span>}
-      </div>
+      </form>
     </header>
   )
 
